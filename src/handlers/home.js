b01cc5f80c76ec5568a459ff25256121309e482d
Bart Bucknill <bartbucknill@hotmail.com>
2017-09-29 13:05:19 +0100

    #4 Make events card for event list

--- src/handlers/home.js
+++ src/handlers/home.js
@@ -1,5 +1,26 @@
-const getTitle = () => 'Home'
+const getEvent = () => {
+  return [
+    {
+      title: 'Sheffield Soup',
+      location: 'Sheffield Arena',
+      startDate: '20 December 2017',
+      ticketsAvailable: '4'
+    },
+    {
+      title: 'Dog',
+      location: 'Main Street',
+      startDate: '15 December 2017',
+      ticketsAvailable: '10'
+    },
+    {
+      title: 'Sheffield Soup',
+      location: 'Sheffield Arena',
+      startDate: '20 December 2017',
+      ticketsAvailable: '4'
+    }
+  ]
+}
 
 module.exports = {
-  getTitle
+  getEvent
 }
