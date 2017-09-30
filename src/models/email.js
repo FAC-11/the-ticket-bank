dd67ca07daa51b422e8e292c24e681ffd58cb968
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-09-30 02:06:49 +0100

    Add travis working with test

--- src/models/email.js
+++ src/models/email.js
@@ -1,17 +1,19 @@
-// so we can use POSTMARK_KEY
-// require('env2')('./config.env');
+// if environment is not travis, require config.env
+if (process.env.TRAVIS !== true) {
+  require('env2')('./config.env')
+}
 
 // require postmark
 const postmark = require('postmark')
 
 // create new client instance:
-// const emailClient = new postmark.Client(process.env.POSTMARK_KEY)
+const emailClient = new postmark.Client(process.env.POSTMARK_KEY)
 
 // for testing
 const testEmailClient = new postmark.Client(process.env.POSTMARK_TEST_KEY)
 
 module.exports = {
-  // emailClient: emailClient,
+  emailClient: emailClient,
   testEmailClient: testEmailClient
 }
 
