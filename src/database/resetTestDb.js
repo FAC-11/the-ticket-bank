bd2edbb88838b7c496b403b81d3c343dd08077eb
Yahia <zaky20@hotmail.com>
2017-10-03 11:45:38 +0100

    created test database and boilerplate for writing all tests. relates #29 #14

--- src/database/resetTestDb.js
+++ src/database/resetTestDb.js
@@ -4,7 +4,7 @@ const sql = require('./index');
 module.exports = () => {
   return queryDb(sql.dbSchema)
     .then(res => {
-       queryDb(sql.testData)
+      queryDb(sql.testData)
     })
     .catch(console.log)
 }
