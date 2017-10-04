441232a94a15ad55e235ae7febe500c83d544e71
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-10-04 14:55:08 +0100

    Make tests pass relates #3

--- tests/test.js
+++ tests/test.js
@@ -5,32 +5,26 @@ const { hashPassword, checkPassword } = require('../src/models/encrypt.js')
 tape('hashPassword', (t) => {
   let plaintextPwd = 'myinsecurepass'
   hashPassword(plaintextPwd)
-  .then((hash) => {
-    t.equal(hash.length, 60, 'length of hash should be 60 characters')
-    t.equal(hash.substring(0,7), '$2a$10$', 'is a bcrypt hash')
-    t.notEqual(hash, plaintextPwd, 'hash should not equal original password')
-    t.end()
-  })
+    .then((hash) => {
+      t.equal(hash.length, 60, 'length of hash should be 60 characters')
+      t.equal(hash.substring(0, 7), '$2a$10$', 'is a bcrypt hash')
+      t.notEqual(hash, plaintextPwd, 'hash should not equal original password')
+      t.end()
+    })
 })
-
-tape('checkPassword', (t) => {
-  
-})
-
-
 
 tape('Postmark', (t) => {
   testEmailClient.sendEmail({
-    "From": "steve@ticketsforgood.co.uk",
-    "To": "test@someone.com",
-    "Subject": "test",
-    "TextBody": "test"
-  }).
-    then((res) => {
+    'From': 'steve@ticketsforgood.co.uk',
+    'To': 'test@someone.com',
+    'Subject': 'test',
+    'TextBody': 'test'
+  })
+    .then((res) => {
       t.equal(res.ErrorCode, 0, 'should send outbound emails')
-    }).
-    catch((err) => {
+    })
+    .catch((err) => {
       t.fail(err.message)
     })
-    t.end()
+  t.end()
 })
