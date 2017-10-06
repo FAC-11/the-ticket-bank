e2a83cf7dee009cd3dfc358927d6ac6f6534166f
Bart Bucknill <bartbucknill@hotmail.com>
2017-10-06 10:54:29 +0100

    #4 Change handlers.home.test to check array length

--- tests/handlers.home.test.js
+++ tests/handlers.home.test.js
@@ -4,18 +4,28 @@ const queryDb = require('../src/database/queryDb.js')
 const sql = require('../src/database/index.js')
 const resetTestDb = require('../src/database/resetTestDb')
 
-const expected = [ { title: 'Fac Welcome', 
-short_desc: 'Welcome event for facsters',
- venue: 'Fac HQ', location: 'Palmers Road', 
- event_date: '03/10/2017', 
- tkts_available: 5 }, 
- { title: 'Fac End', short_desc: 'Leaving event for facsters',
-  venue: 'Fac HQ', location: 'Palmers Road', event_date: '03/10/2017',
-   tkts_available: 3 }, { title: 'beiber', short_desc: 'concert',
-    venue: 'Fac HQ', location: 'Palmers Road', event_date: '03/10/2017',
-     tkts_available: 4 }, { title: 'Haloween', short_desc: 'party', 
-     venue: 'Fac HQ', location: 'Palmers Road', event_date: '03/10/2017',
-      tkts_available: 3 } ]
+const expected = [ { title: 'Fac Welcome',
+  short_desc: 'Welcome event for facsters',
+  venue: 'Fac HQ',
+location: 'Palmers Road',
+  event_date: '03/10/2017',
+  tkts_available: 5 },
+{ title: 'Fac End',
+short_desc: 'Leaving event for facsters',
+  venue: 'Fac HQ',
+location: 'Palmers Road',
+event_date: '03/10/2017',
+  tkts_available: 3 }, { title: 'beiber',
+short_desc: 'concert',
+  venue: 'Fac HQ',
+location: 'Palmers Road',
+event_date: '03/10/2017',
+  tkts_available: 4 }, { title: 'Haloween',
+short_desc: 'party',
+  venue: 'Fac HQ',
+location: 'Palmers Road',
+event_date: '03/10/2017',
+  tkts_available: 3 } ]
 
 tape('Test handlers/home', (t) => {
   resetTestDb()
@@ -23,10 +33,10 @@ tape('Test handlers/home', (t) => {
       return getEvents()
     })
     .then(actual => {
-      t.equal(Array.isArray(actual), true, 'Response should be an array')
+      t.equal(actual.length, expected.length, 'Actual array length should equal expected array length')
       t.equal(actual[0].title, expected[0].title, 'Array elements should include an event title')
-      t.equal(actual[1].short_desc, expected[1].short_desc, 'Array elements should include an event short description')  
-      t.end()    
+      t.equal(actual[1].short_desc, expected[1].short_desc, 'Array elements should include an event short description')
+      t.end()
     })
     .catch(console.error)
 })
