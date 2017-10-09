ad7570c248a18a627dd05b393f2ad8845a661a82
Yahia <zaky20@hotmail.com>
2017-10-09 11:53:01 +0100

    error messages for invalid email/password

--- src/handlers/verifyLogin.js
+++ src/handlers/verifyLogin.js
@@ -5,12 +5,20 @@ const sql = require('../database/index')
 const verifyLogin = (req, res) => {
   return queryDb(sql.getHashedPassword, [req.body.email])
     .then(hashedPassword => {
-      if (!hashedPassword.length) { console.log('email not found') }
+      if (!hashedPassword.length) {
+        res.render('login', {
+          errorEmail: 'Email not found'
+        })
+      }
       return bcrypt.compare(req.body.password, hashedPassword[0].password)
     })
     .then(bool => {
       if (bool) return queryDb(sql.getUserDetails, [req.body.email])
-      else console.log('passwords dont match')
+      else {
+        res.render('login', {
+          errorPassword: 'Wrong Password'
+        })
+      }
     })
     .then(userDetails => {
       req.session.userId = userDetails[0].id
