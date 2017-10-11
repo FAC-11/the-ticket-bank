19849f3af3612839b445b0ee09e49f4b48af46e5
Yahia <zaky20@hotmail.com>
2017-10-11 16:39:04 +0100

    adapted number of forms rendered based on number of tickets a charity requests when applying

--- src/routes/applyForTickets.js
+++ src/routes/applyForTickets.js
@@ -1,4 +1,22 @@
-module.exports = (req, res) => {
-  res.render('applyForTickets',{max_allocation:3,
-    permittedTicket: new Array(3).fill(1) })
+const { getSingleEvent } = require('../handlers/event')
+
+module.exports = (req, res, next) => {
+  getSingleEvent(req.params)
+    .then(event => {
+      if (!event) {
+        next()
+      } else {
+        const session = {
+          userId: req.session.userId || null,
+          isAdmin: req.session.isAdmin || null
+        }
+        console.log('reqparticipants', typeof req.body.numberOfParticipants)
+        res.render('applyForTickets', {
+          max_allocation: event.max_allocation,
+          permittedTicket: new Array(parseInt(req.body.numberOfParticipants)).fill(1)
+        })
+      }
+    })
+    .catch(console.error)
 }
+
