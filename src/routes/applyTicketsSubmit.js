00ca187966d08e6ab50b52b8844387d609b9dd37
Bart Bucknill <bartbucknill@hotmail.com>
2017-10-13 00:52:35 +0100

    #2 Render successful ticket application page

--- src/routes/applyTicketsSubmit.js
+++ src/routes/applyTicketsSubmit.js
@@ -8,13 +8,18 @@ module.exports = (req, res) => {
   const participantArr = extractTicketParticipants(req)
   sendCharityApplyTicketSuccess(participantArr)
   emailAdminApplyTicketSuccess(participantArr)
+  const session = {
+    userId: req.session.userId || null,
+    isAdmin: req.session.isAdmin || null
+  }
+  res.render('applySuccess', { session })
 }
-  // validateTicketForm(req)
-  // .then(()=> {
-  //   for (req.body.numberoftickets)
-  // })
-  //   .then(() => console.log(req.body))
-  //   applyTicket(req.body))
+// validateTicketForm(req)
+// .then(()=> {
+//   for (req.body.numberoftickets)
+// })
+//   .then(() => console.log(req.body))
+//   applyTicket(req.body))
 //     .then(event => {
 //       if (!event) {
 //         throw new Error('There was a problem adding the event to the database.')
