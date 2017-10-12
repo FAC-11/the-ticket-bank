13479bd57237f0423516d2cd6207ec57f1ede20b
Bart Bucknill <bartbucknill@hotmail.com>
2017-10-12 18:22:11 +0100

    #2 Email charity when ticket successfully applied for

---
+++ src/models/emailCharityApplyTicketSuccess.js
@@ -0,0 +1,48 @@
+const { emailClient } = require('../models/email.js')
+const fetchEmailFromCharityName = require('../database/sql-queries/fetchEmailFromCharityName')
+
+const returnSingleEmail = (eventTitle, charityEmail, numberOfTickets) => {
+  return {
+    'From': 'steve@ticketsforgood.co.uk',
+    'To': `${charityEmail}`,
+    'Subject': 'New Tickets Available!',
+    'TextBody': `
+    Hi,
+    You have successfully applied for ${numberOfTickets} tickets for ${eventTitle}.
+    Steve from Tickets for good will be in touch with information about your tickets.
+
+    Check out all our available event tickets here: http://0.0.0.0:4000/
+
+    Have a good day!`
+  }
+}
+
+const sendCharityApplyTicketSuccess = (participantsArr) => {
+  const { nameOfCharity, eventTitle } = participantsArr[0]
+  return fetchEmailFromCharityName(nameOfCharity)
+    .then(emailObjArr => {
+      const email = returnSingleEmail(eventTitle, emailObjArr[0].email, participantsArr.length)
+      return new Promise((resolve, reject) => {
+        emailClient.sendEmail(email, (err, res) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(res)
+          }
+        })
+      })
+    })
+    .then(result => {
+      return result
+    }) // TODO: send email notification to admin, emails successfully sent
+    .catch(err => {
+      console.err(err)
+      return err
+    }) // TODO: send email notification to admin, error sending new email notification
+}
+
+module.exports = {
+  returnSingleEmail,
+  returnListOfEmails,
+  sendNewEventEmails
+}
